7f3502c98b7b34a5b25d4e3774f5b8a9e929b43d
Maeve <mfitz004@gmail.com>
2022-01-28 18:40:03 -0800

    Added playRound function

--- rockps.js
+++ rockps.js
@@ -1,11 +1,33 @@
 function computerPlay() {
-    let motion = Math.random();
-    if (motion <= 0.33) {
-        return "rock";
-    }    else if (0.33 < motion <= 0.66) {
-        return "paper";
-    }    else if (0.66 < motion <= 1) {
-        return "scissors";
+        let motion = Math.random();
+        if (motion <= 0.33) {
+            return "rock";
+        }    else if (motion <= 0.66) {
+            return "paper";
+        }    else if (motion <= 1) {
+            return "scissors";
+        }
     }
+
+
+function gameRound(playerSelection, computerSelection) {
+    console.log(computerSelection);
+    if (computerSelection == "rock") {
+        return "It's a tie! Try again.";
+    }    else if (computerSelection == "paper") {
+        return "Paper beats Rock, you lose!";
+    }     else if (computerSelection == "scissors") {
+        return "Rock beats Scissors, you win!";
+    }
+    
 }
-console.log(computerPlay());
+
+const playerSelection = "rock".toLowerCase;
+const computerSelection = computerPlay();
+console.log(gameRound(playerSelection, computerSelection));
+
+
+
+
+
+
